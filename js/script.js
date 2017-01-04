b073b3768ccf09001a326fb16e7aa5aaed1244ee
Jef Roosens <jef.roosens@gmail.com>
2017-01-04 11:00:07 +0100

    changes

--- js/script.js
+++
@@ -1,7 +0,0 @@
-$('.work img').mouseover(function() {
-    $('.project_overlay').css("visibility","visible");
-});
-
-$('.work').mouseout(function() {
-    $('.project_overlay').css("visibility","hidden");
-});
